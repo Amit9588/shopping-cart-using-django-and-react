ec68479925e70ce2999588aac0d71c51211160eb
Amit9588 <phenomenalreigns3@gmail.com>
2023-03-27 13:48:29 +0530

    customise,serializing,authenticating the order section

--- ecom/api/order/models.py
+++ ecom/api/order/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from api.user.models import CustomUser
+from api.product.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,null=True)
+
+    product_names = models.CharField(max_length=500)
+    total_product = models.CharField(max_length=500, default=0)
+    transaction_id = models.CharField(max_length=150,default=0)
+    total_amount = models.CharField(max_length=50,default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updatedat = models.DateTimeField(auto_now=True)

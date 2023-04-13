9eb469da1b563be934f5664aaba83c24988ddb01
Amit9588 <phenomenalreigns3@gmail.com>
2023-04-13 21:27:55 +0530

    configuring cart and auth helper

---
+++ projectfrontend/src/auth/helper/index.js
@@ -0,0 +1,77 @@
+import {API} from "../../backend"
+import {cartEmpty} from "../../core/helper/cartHelper"
+
+export const signup = user => {
+    return fetch(`${API}user/`, {
+        method: "POST",
+        headers : {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then((response) => {
+        return response.json()
+
+    })
+    .catch (err => console.log(err))
+
+}
+
+export const signin = user => {
+    const fromData = new FormData()
+
+    for (const name in user) {
+        fromData.append(name,user[name ])
+    }
+
+    return fetch(`${API}user/login`,{
+        method: "POST",
+        body: FormData
+
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+};
+
+
+export const authenticate = (data, next) => {
+    if (typeof window !== undefined) {
+        localStorage.setItem("jwt",JSON.stringify(data))
+        next();
+    }
+};
+
+export const isAuthenticated = () => {
+    if (typeof window !== undefined) {
+        return false
+    }
+    if (localStorage.getItem("jwt")){
+        return JSON.parse(localStorage.getItem("jwt"))
+
+    } else {
+        return false
+    }
+};
+
+export const signout = next => {
+    const userId = isAuthenticated() && isAuthenticated().user.id
+
+    if (typeof window !== undefined){
+        localStorage.removeItem("jwt")
+        cartEmpty(() => {})
+        //next();
+
+        return fetch(`${API}user/logout/${userId}`, {
+            method: "GET"
+        })
+        .then(response => {
+            console.log("Signout Success")
+            next();
+        })
+        .catch(err => console.log(err))
+        
+    }
+}
--- projectfrontend/src/core/Card.js
+++ projectfrontend/src/core/Card.js
@@ -1,38 +1,81 @@
 import React from "react"
 import ImageHelper from "./helper.js/imagehelper";
+import { Navigate } from "react-router-dom";
+import { addItemtoCart,removeItemfromCart } from "./helper.js/cartHelper";
+
+
+// to deal with this later
+const isAuthenticated = true;
 
 const Card = ({
     product,
     addtocart= true,
-    removefromcart = false
+    removefromCart = true
 
 }) => {
+
+  const cartTitle = product ? product.name :"A photo from pixel"
+  const cartDescription = product ? product.description :"Default description"
+  const cartPrice = product ? product.price :"Default"
+
+  const addToCart=() => {
+    if (isAuthenticated){
+      addItemtoCart(product, ()=> {})
+      console.log("Added to cart")
+    }else {
+      console.log ("Login Please")
+      }
+    };
+  const getAredirect = Navigate => {
+    if (Navigate) {
+      return <Navigate to= "/cart" />
+      }
+    };
+  const showAddToCart = addToCart =>{
+    return(
+      addtocart &&(
+        <button
+              onClick={() => {addToCart()}}
+              className="btn btn-block btn-outline-success mt-2 mb-2"
+              >
+              Add to Cart
+          </button>
+        )
+      )
+    }
+
+    const showRemovefromCart = removefromCart => {
+      return(
+        removefromCart && (
+          <button
+                onClick={() => {
+                  removeItemfromCart(product._id);
+                  console.log("Product Handle form Cart")
+                }}
+                className="btn btn-block btn-outline-danger mt-2 mb-2"
+              >
+                Remove from cart
+          </button>
+        )
+      )
+    }
     return (
       <div className="card text-white bg-dark border border-info ">
-        <div className="card-header lead">A photo from pexels</div>
+        <div className="card-header lead">{cartTitle}</div>
+
         <div className="card-body">
            < ImageHelper product={product}/>
           
           <p className="lead bg-success font-weight-normal text-wrap">
-            this photo looks great
+            {cartDescription}
           </p>
-          <p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
+          <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
           <div className="row">
             <div className="col-12">
-              <button
-                onClick={() => {}}
-                className="btn btn-block btn-outline-success mt-2 mb-2"
-              >
-                Add to Cart
-              </button>
+              {showAddToCart(addToCart)}
             </div>
             <div className="col-12">
-              <button
-                onClick={() => {}}
-                className="btn btn-block btn-outline-danger mt-2 mb-2"
-              >
-                Remove from cart
-              </button>
+              {showRemovefromCart(removefromCart)}
             </div>
           </div>
         </div>
